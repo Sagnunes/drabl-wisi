55e399516f561852e728897d5f03c723117bb4e8
Sérgio Nunes <sagnunes@gmail.com>
2025-06-15 21:41:49 +0100

    Add a Role model, migration, factory, seeder, and policy with associated resource controller and requests

---
+++ app/Http/Requests/Roles/StoreRoleRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\Roles;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRoleRequest extends BaseRoleRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return array_merge(parent::rules(), []);
+    }
+}
